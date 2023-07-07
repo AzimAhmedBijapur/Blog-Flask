44475870394465ec3404449db1d44d5704574d4b
ThunderBoltCodes <94512886+azimbijapur786@users.noreply.github.com>
2023-07-07 13:10:58 +0530

    sign in form connected to userdb

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, request
 import json
+import MySQLdb
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+local = True
+
 with open('config.json') as file:
     param = json.load(file)["params"]
+
+if local:
+    app.config['SQLALCHEMY_DATABASE_URI'] = param['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = param['production_uri']
+
+
+userdb = SQLAlchemy(app)
+
+
+class User(userdb.Model):
+    sno = userdb.Column(userdb.Integer, primary_key=True)
+    fname = userdb.Column(userdb.String(80))
+    lname = userdb.Column(userdb.String(80))
+    email = userdb.Column(userdb.String(120), unique=True)
+    password = userdb.Column(userdb.String(15), unique=True)
 
 
 @app.route('/')
@@ -12,9 +33,19 @@ def index():
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template('sign-in.html')
+    if request.method == 'GET':
+        return render_template('sign-in.html')
+
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    new_user = User(fname=fname, lname=lname, email=email, password=password)
+    userdb.session.add(new_user)
+    userdb.session.commit()
+    return render_template('login.html')
 
 
 @app.route('/home')

1d69c9cc5597a7e014daaf120d0ac803f352d102
ThunderBoltCodes <94512886+azimbijapur786@users.noreply.github.com>
2023-07-13 13:48:45 +0530

    click on the post to view the contents, added timestamp and author name to the posts

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ import json
 import smtplib
 import ssl
 import psycopg2
-from io import BytesIO
-from PIL import Image
 from flask import Flask, render_template, request, flash, redirect
-import base64
-import os
-import imghdr
-from uuid import uuid1
-from werkzeug.utils import secure_filename
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -45,11 +39,13 @@ except Exception as e:
 try:
     cur.execute("""CREATE TABLE IF NOT EXISTS posts(
         id serial PRIMARY KEY,
+        name TEXT,
         email VARCHAR(120),
         password VARCHAR(15),
         title VARCHAR(255),
+        description TEXT,
         content TEXT,
-        img BYTEA)
+        time TIMESTAMP);
     """)
     conn.commit()
     print('Successfully created table posts')
@@ -70,10 +66,8 @@ def index():
     user = cur.fetchone()
     if user is None:
         return render_template('login.html')
-    fname = user[1]
-    lname = user[2]
     conn.commit()
-    return render_template('home.html', name=f"Welcome {fname} {lname}")
+    return redirect('/home')
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -96,28 +90,11 @@ def register():
     return render_template('login.html')
 
 
-def get_format(bytea):
-    # decoded_string = bytes(base64.b64encode(bytea).decode('utf-8'))
-    encoded_bytes = bytes(bytea)
-    extension = imghdr.what(None, h=encoded_bytes)
-    return extension
-
-
-def decode_bytea(bytea):
-    return base64.b64encode(bytea).decode('utf-8')
-
-
-app.jinja_env.filters.update(get_format=get_format)
-
-
-app.jinja_env.filters.update(decode_bytea=decode_bytea)
-
-
 @app.route('/home')
 def home():
     try:
         cur.execute('SELECT * FROM POSTS;')
-        posts = cur.fetchall();
+        posts = cur.fetchall()
         conn.commit()
     except Exception as ex:
         print('Could not fetch from the db', ex)
@@ -150,24 +127,39 @@ def blog():
         return render_template('blog.html')
     try:
         email = request.form.get('email')
+        name = request.form.get('name')
         password = request.form.get('password')
         title = request.form.get('title')
         content = request.form.get('content')
-        img = request.files['img']
-        img_file = img.read()
-        encoded_image = base64.b64encode(img_file)
+        description = request.form.get('description')
+        time = datetime.now()
         cur.execute("select id,fname,lname from users where email=%s and password=%s;", (email, password))
         user = cur.fetchone()
         if user is None:
             flash('Invalid email or password')
             return render_template('blog.html')
-        cur.execute("INSERT INTO posts (email, password, title, content, img) VALUES (%s, %s, %s, %s, %s);",
-                    (email, password, title, content, encoded_image))
+        cur.execute("INSERT INTO posts (name,email, password, title, description, content, time) "
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s);",
+                    (name, email, password, title, description, content, time))
         conn.commit()
         print('Successfully inserted into posts')
     except Exception as ex:
-        print('Could not insert to posts database',ex)
+        print('Could not insert to posts database', ex)
     return render_template('blog.html')
+
+
+cur.execute("SELECT id, name, title, content, time FROM posts;")
+posts = cur.fetchall()
+conn.commit()
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    for post in posts:
+        print(post)
+        if post[0] == post_id:
+            return render_template('post.html', title=post[2], content=post[3], name=post[1], time=post[4])
+    return "Post not found"
 
 
 app.run(debug=True)
